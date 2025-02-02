b3f7b8edb94da33e3fb1f314cbaa9bcba79a3254
KimGona <kimgona1416@gmail.com>
2025-02-02 19:19:22 +0900

    W4M2 진행 중

--- Missions/Week04/M2/data_transform.py
+++ Missions/Week04/M2/data_transform.py
@@ -4,9 +4,6 @@ from pyspark.sql import functions as F
 # Parquet 파일 읽기
 df_parquet = spark.read.parquet('data/fhvhv_tripdata_2023-12.parquet')
 
-# 결측값 처리 (예시: 결측값이 있는 행 삭제)
-df_cleaned = df_csv.dropna()
-
 # 시간 포맷 변환 (예: 'pickup_datetime'을 Timestamp로 변환)
 df_cleaned = df_cleaned.withColumn("pickup_datetime", to_timestamp("pickup_datetime", "yyyy-MM-dd HH:mm:ss"))
 
