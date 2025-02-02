7ebce81c01b0a3f55ddd8710cb898f851b206ad8
KimGona <kimgona1416@gmail.com>
2025-02-02 16:54:55 +0900

    W4M1 pi.py코드 수정

--- Missions/Week04/M1/pi.py
+++ Missions/Week04/M1/pi.py
@@ -5,6 +5,11 @@ from operator import add
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
+def f(_: int) -> float:
+    x = random() * 2 - 1
+    y = random() * 2 - 1
+    return 1 if x ** 2 + y ** 2 <= 1 else 0
+
 if __name__ == "__main__":
 
     spark = SparkSession\
@@ -12,24 +17,27 @@ if __name__ == "__main__":
         .appName("PythonPi")\
         .getOrCreate()
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
+    #partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    #n = 100000 * partitions
 
-    def f(_: int) -> float:
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+    n = 1000000 # 100만 개
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    count = spark.sparkContext.parallelize(range(n)).map(f).reduce(lambda a, b: a + b)
     pi = 4.0 * count / n
     print("Pi is roughly %f" % pi)
 
     # 결과를 DataFrame으로 변환
-    result = Row(num_samples=n, pi_estimate=pi)
-    df = spark.createDataFrame([result])
+    df = spark.createDataFrame([(pi,)], ["Estimated Pi"])
+
+    #result = Row(num_samples=n, pi_estimate=pi)
+    #df = spark.createDataFrame([result])
 
     # CSV로 저장
     output_path = "/opt/spark/output/pi_estimate.csv"  # 저장할 경로
     df.write.mode("overwrite").csv(output_path, header=True)
+    #df.coalesce(1).write.mode("overwrite").format("csv").option("header", "true").save(output_path)
+    #df.write.csv(output_path, mode='overwrite', header = True)
+    #df.coalesce(1).write.csv(output_path, mode="overwrite") csv 밑에 _SUCCESS 생김
+    #df.write.mode("overwrite").csv(output_path, header=True) csv 밑에 _SUCCESS 생김
 
     spark.stop()   
